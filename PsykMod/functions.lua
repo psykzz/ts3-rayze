da92420e58f54720edfd902478df5836828b6053
Matt Smith <matt.daemon660@gmail.com>
2012-09-13 20:25:53 +0100

    ~ Fixed random not being random enough
    Updated the random function to use a incremental seed based on current
    time.

--- PsykMod/functions.lua
+++ PsykMod/functions.lua
@@ -9,6 +9,9 @@
 -- Verbose mode : prints lots of debug text.
 local verbose = false
 
+-- Starting seed 
+local seed = os.time()
+
 function msg(txt)
    if verbose then
       ts3.printMessageToCurrentTab(txt)
@@ -123,52 +126,13 @@ function moveClient (ServerConn, clientID, newChannelID, password)
 end
 
 function rand(lower,upper)
-   math.randomseed(tonumber(tostring(os.clock()):reverse():sub(1,6)))
+   seed = seed+1
+   math.randomseed(tonumber(tostring(seed):reverse():sub(1,6)))
    math.random()
    math.random()
    math.random()
-   math.randomseed(tonumber(tostring(os.clock()):reverse():sub(1,6))+math.random())
+   math.randomseed(tonumber(tostring(seed):reverse():sub(1,6))+math.random())
    math.random()
    math.random()
    return math.random(lower,upper)
-end
-
-function scramble(serverConnectionHandlerID, ...)
-   subscribeAllChannels(serverConnectionHandlerID) 
-   local myClientID = getSelfID(serverConnectionHandlerID)
-   local clients = getClientList(serverConnectionHandlerID)
-   local myClientChannel = getChannelID( serverConnectionHandlerID, myClientID )
-   local channels = getChannelList(serverConnectionHandlerID)
-   for k,v in ipairs(clients) do
-      local rand = rand(1,#channels)
-      local clientChannel = getChannelID( serverConnectionHandlerID, v )
-      if clientChannel ~= myClientChannel then
-         print(""..v.."removed")
-         msg('removed '.. v ..' because not in channel') 
-         table.remove(clients,k)
-      else
-         local clientName = getClientName(serverConnectionHandlerID, clients[k])
-         moveClient(serverConnectionHandlerID,v,channels[rand], "")
-         sendServerMessage(serverConnectionHandlerID, "[b]Scramble![/b]")
-      end
-   end
-end 
-
-function roll(serverConnectionHandlerID, ...)
-   subscribeAllChannels(serverConnectionHandlerID) 
-   local myClientID = getSelfID(serverConnectionHandlerID)
-   local clients = getClientList(serverConnectionHandlerID)
-   local myClientChannel = getChannelID( serverConnectionHandlerID, myClientID )
-   for k,v in ipairs(clients) do 
-      local clientChannel = getChannelID( serverConnectionHandlerID, v )
-      if clientChannel ~= myClientChannel then
-         msg('removed '.. v ..' because not in channel') 
-         table.remove(clients,k)
-      end
-   end
-   local rand = rand(1,#clients)
-   local clientName = getClientName(serverConnectionHandlerID, clients[rand])
-   sendServerMessage(serverConnectionHandlerID, "[b]DICE ROLLED![/b]")
-   kickFromChannel(serverConnectionHandlerID, clients[rand], "Lady luck has struck again!")
-   sendServerMessage(serverConnectionHandlerID, "[b]Dice rolled a number "..rand.."! "..clientName.." was Kicked, unlucky![/b]")
 end
